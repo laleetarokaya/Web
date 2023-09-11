a23bf28d0c50d8ae8c58583ca54cc78cb2b28112
laleetarokaya <laleeta2056@gmail.com>
2023-09-11 17:04:05 +0545

    java

---
+++ java/index.js
@@ -0,0 +1,35 @@
+// console.log("external js connected 1.")
+// alt+tab=multiple tab
+// ctrl+/ line comment
+// ctrl +shift +a multi line comment
+
+greeting = "Namaste"
+/*
+console.log("Hello Ram");
+console.log("Hello Sanjaya");
+console.log("Hello tekendra");
+console.log("Hello Sita");
+console.log("Hello Gita");
+*/
+
+console.log(greeting, "Sanjaya");
+console.log(greeting, "tekendra");
+console.log(greeting, "Sita");
+console.log(greeting, "Gita");
+console.log(greeting, "Ram");
+
+
+firstName = "Sanjaya"
+lastName = "Shahi"
+age = 20
+address = "Birendranagar" + "surkhet"
+full_name = firstName + lastName //Sanjaya Shahi // snake_case
+fullName = firstName + lastName // Sanjaya Shahi  // camelCase
+
+ console.log("Sanjaya", "Shahi")
+ console.log("fullName is", fullName)
+ console.log("full_name is", full_name)
+ console.log(20)
+ console.log("age is", age)
+// console.log("Sanjaya", "Shahi") alternative way
+console.log("address is " + address)
